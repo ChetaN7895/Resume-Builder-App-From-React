8c6a4000edfc289fe0d038f83f216abc8df9e13a
Chetan Solanki <151900157+ChetaN7895@users.noreply.github.com>
2024-11-01 14:09:36 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import Container from 'react-bootstrap/Container';
 function Footer() {
   return (
     <Container fluid  className="border-top mt-2 text-center bg-light text-dark py-3">
-        <p className="m-0">Resume Builder</p>
+        <p class="m-0">Resume Builder | copyright ©| Chetan Solanki </p>
     <p>***Chetan-Solanki***</p>
     </Container>
   )
